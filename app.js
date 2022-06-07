0aba44bfc3d139b8c0825bf3c986a24b4667b649
tecosama <taylancankose@gmail.com>
2022-06-07 12:18:35 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-const fn = function () {
-  console.log('Function is working...');
-};
+const express = require('express');
+const path = require('path');
 
-fn();
+const app = express();
+
+/* MIDDLEWARE: NODEJS temelde bir request gönderir ve ona yönelik bir response alır. Yani request-response cycle
+ Req-res arasındaki yere middle denir */
+
+/* MIDDLEWARES */
+const myLogger = (req, res, next) => {
+  console.log('Middleware log 1');
+  next(); /* bir sonraki middlewareye ilerlemesi için next'i çağırmamız lazım */
+};
+const myLogger2 = (req, res, next) => {
+  console.log('Middleware log 2');
+  next(); /* bir sonraki middlewareye ilerlemesi için next'i çağırmamız lazım */
+};
+/* MIDDLEWARES */
+
+app.use(express.static('public'));
+app.use(myLogger);
+app.use(myLogger2);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, (err) => {
+  console.log(`Sunucu ${port} portunda başlatıldı...`);
+});
